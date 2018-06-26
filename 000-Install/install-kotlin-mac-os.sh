1fd6ae4a43a0f23f096c7b8bdba5f985e972b018
Evgeny Trushin <evgeny.trushin@inteliment.com>
2018-06-26 18:41:03 +1000

    MacOS install script was added.

---
+++ 000-Install/install-kotlin-mac-os.sh
@@ -0,0 +1,4 @@
+#readme https://kotlinlang.org/docs/tutorials/command-line.html
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install kotlin
