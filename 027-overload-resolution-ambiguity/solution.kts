4bcb5b4a4274e29c3ee6bc6c6a6855f69cb9003a
Evgeny Trushin <evgeny.trushin@inteliment.com>
2018-08-24 08:20:29 +1000

    [new] A new error "overload resolution ambiguity".

---
+++ 027-overload-resolution-ambiguity/solution.kts
@@ -0,0 +1,6 @@
+//error: overload resolution ambiguity: public constructor Info(nameA: String) defined in Fixme.Info
+data class Info(val nameB: String) {
+    constructor(nameA: String) : this(nameB = nameA)
+}
+
+println("a equals ${Info(nameA = "nameA").nameB}")
